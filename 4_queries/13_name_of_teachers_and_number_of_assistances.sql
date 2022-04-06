8dfecf3d8b0a68945588b2ae96f596b0d60da669
Rachel PR <rplourder@gmail.com>
2022-04-05 23:03:01 -0300

    A query to get total assistances for a specific cohort and all teachers

---
+++ 4_queries/13_name_of_teachers_and_number_of_assistances.sql
@@ -0,0 +1,9 @@
+SELECT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests.id)
+  FROM teachers
+  JOIN assistance_requests ON teachers.id = teacher_id
+  JOIN students ON students.id = student_id
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name = 'JUL02'
+  GROUP BY teachers.name, cohorts.name
+  ORDER BY teachers.name
+  ;
