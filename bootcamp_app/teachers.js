c0925810597d9d4139b7718c400b1b3aa09f5bf0
Rachel PR <rplourder@gmail.com>
2022-04-09 12:59:06 -0300

    Fix code to use parameterized queries to prevent sql injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,21 +7,22 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
-pool
-  .query(
-    `
+const queryString = `
 SELECT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = 'JUL02'
+WHERE cohorts.name LIKE $1 
 GROUP BY teachers.name, cohorts.name
 ORDER BY teachers.name;
-`
-  )
-  .then((res) => {
-    res.rows.forEach((row) => {
-      console.log(`${row.cohort} : ${row.teacher}`);
-    });
+`;
+const cohortName = process.argv[2];
+
+const values = [`%${cohortName}%`];
+
+pool.query(queryString, values).then((res) => {
+  res.rows.forEach((row) => {
+    console.log(`${row.cohort} : ${row.teacher}`);
   });
+});
